e467034d63e7ed3279e55cf8d59bafe2b6bd60ef
atrme <ischaoyangy@gmail.com>
2025-01-22 18:21:30 +0800

    Complete discussion07

---
+++ discussion/disc07/bear.py
@@ -0,0 +1,55 @@
+class Eye:
+    """An eye.
+
+    >>> Eye().draw()
+    '0'
+    >>> print(Eye(False).draw(), Eye(True).draw())
+    0 -
+    """
+    def __init__(self, closed=False):
+        self.closed = closed
+
+    def draw(self):
+        if self.closed:
+            return '-'
+        else:
+            return '0'
+
+class Bear:
+    """A bear.
+
+    >>> Bear().print()
+    ? 0o0?
+    """
+    def __init__(self):
+        self.nose_and_mouth = 'o'
+
+    def next_eye(self):
+        return Eye()
+
+    def print(self):
+        left, right = self.next_eye(), self.next_eye()
+        print('? ' + left.draw() + self.nose_and_mouth + right.draw() + '?')
+
+class SleepyBear(Bear):
+    """A bear with closed eyes.
+
+    >>> SleepyBear().print()
+    ? -o-?
+    """
+    def next_eye(self):
+        return Eye(True)
+
+class WinkingBear(Bear):
+    """A bear whose left eye is different from its right eye.
+
+    >>> WinkingBear().print()
+    ? -o0?
+    """
+    def __init__(self):
+        self.closed = False
+        super().__init__()
+
+    def next_eye(self):
+        self.closed ^= True
+        return Eye(self.closed)

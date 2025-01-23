5910198381b542350a87223e500e6a8de7287ca2
atrme <ischaoyangy@gmail.com>
2025-01-23 20:57:54 +0800

    Complete homework06

---
+++ homework/hw06/hw06.py
@@ -0,0 +1,235 @@
+passphrase = 'REPLACE_THIS_WITH_PASSPHRASE'
+
+def midsem_survey(p):
+    """
+    You do not need to understand this code.
+    >>> midsem_survey(passphrase)
+    'f65fb8fdaeda6d85eb3089dcdf7784836dde30e260c0ad31b9b2e533'
+    """
+    import hashlib
+    return hashlib.sha224(p.encode('utf-8')).hexdigest()
+
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Nothing left to vend. Please restock.'
+    >>> v.add_funds(15)
+    'Nothing left to vend. Please restock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'Please add $10 more funds.'
+    >>> v.add_funds(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'Please add $3 more funds.'
+    >>> v.add_funds(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.add_funds(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.add_funds(15)
+    'Nothing left to vend. Please restock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.add_funds(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    def __init__(self, product, price):
+        """Set the product and its price, as well as other instance attributes."""
+        self.product = product
+        self.price = price
+        self.stock = 0
+        self.fund = 0
+
+    def restock(self, n):
+        """Add n to the stock and return a message about the updated stock level.
+
+        E.g., Current candy stock: 3
+        """
+        self.stock += n
+        message = f'Current {self.product} stock: {self.stock}'
+        return message
+
+    def add_funds(self, n):
+        """If the machine is out of stock, return a message informing the user to restock
+        (and return their n dollars).
+
+        E.g., Nothing left to vend. Please restock. Here is your $4.
+
+        Otherwise, add n to the balance and return a message about the updated balance.
+
+        E.g., Current balance: $4
+        """
+        if self.stock == 0:
+            message = f'Nothing left to vend. Please restock. Here is your ${n}.'
+        else:
+            self.fund += n
+            message = f'Current balance: ${self.fund}'
+        return message
+
+    def vend(self):
+        """Dispense the product if there is sufficient stock and funds and
+        return a message. Update the stock and balance accordingly.
+
+        E.g., Here is your candy and $2 change.
+
+        If not, return a message suggesting how to correct the problem.
+
+        E.g., Nothing left to vend. Please restock.
+              Please add $3 more funds.
+        """
+        if self.stock == 0:
+            message = 'Nothing left to vend. Please restock.'
+        elif self.fund < self.price:
+            message = f'Please add ${self.price - self.fund} more funds.'
+        else:
+            self.stock -= 1
+            self.fund -= self.price
+            if self.fund:
+                message = f'Here is your {self.product} and ${self.fund} change.'
+                self.fund = 0
+            else:
+                message = f'Here is your {self.product}.'
+        
+        return message
+
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    >>> store_digits(2450)
+    Link(2, Link(4, Link(5, Link(0))))
+    >>> store_digits(20105)
+    Link(2, Link(0, Link(1, Link(0, Link(5)))))
+    >>> # a check for restricted functions
+    >>> import inspect, re
+    >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
+    >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
+    """
+    if n // 10 == 0:
+        return Link(n)
+    
+    high_digits = store_digits(n // 10)
+    end = high_digits
+    while True:
+        if end.rest is Link.empty:
+            end.rest = Link(n % 10)  # Add a node to the end of Link
+            return high_digits
+        else:
+            end = end.rest
+
+def deep_map_mut(func, s):
+    """Mutates a deep link s by replacing each item found with the
+    result of calling func on the item. Does NOT create new Links (so
+    no use of Link's constructor).
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> print(link1)
+    <3 <4> 5 6>
+    >>> # Disallow the use of making new Links before calling deep_map_mut
+    >>> Link.__init__, hold = lambda *args: print("Do not create any new Links."), Link.__init__
+    >>> try:
+    ...     deep_map_mut(lambda x: x * x, link1)
+    ... finally:
+    ...     Link.__init__ = hold
+    >>> print(link1)
+    <9 <16> 25 36>
+    """
+    if isinstance(s.first, Link):
+        deep_map_mut(func, s.first)
+    else:
+        s.first = func(s.first)
+
+    if s.rest is not Link.empty:
+        deep_map_mut(func, s.rest)
+
+
+def two_list(vals, counts):
+    """
+    Returns a linked list according to the two lists that were passed in. Assume
+    vals and counts are the same size. Elements in vals represent the value, and the
+    corresponding element in counts represents the number of this value desired in the
+    final linked list. Assume all elements in counts are greater than 0. Assume both
+    lists have at least one element.
+    >>> a = [1, 3]
+    >>> b = [1, 1]
+    >>> c = two_list(a, b)
+    >>> c
+    Link(1, Link(3))
+    >>> a = [1, 3, 2]
+    >>> b = [2, 2, 1]
+    >>> c = two_list(a, b)
+    >>> c
+    Link(1, Link(1, Link(3, Link(3, Link(2)))))
+    """
+    if counts[0] == 1 and len(counts) == 1: # If counts == [1]
+        return Link(vals[0])
+
+    if counts[0] == 1:  # If counts == [1, ...]
+        return Link(vals[0], two_list(vals[1:], counts[1:]))
+
+    counts[0] -= 1
+    return Link(vals[0], two_list(vals, counts))
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
